ed3aee875335c036d007acae6cc06edfcb620dea
dexterai-lab <debabrata.rc91@gmaill.com>
2020-06-18 23:25:53 +0200

    Added search for readme in setup.py

--- quantumdl/setup.py
+++ quantumdl/setup.py
@@ -1,15 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+parent_directory = path.dirname(this_directory)
+with open(path.join(parent_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'quantumdl',         # How you named your package folder (MyLib)
   packages = ['quantumdl'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='Apache License 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Quantum Deep Learning Library!',   # Give a short description about your library
   author = 'Debabrata Roy Chowdhury',                   # Type in your name
   author_email = 'debabrata.rc@dexterai.com',      # Type in your E-Mail
   url = 'https://github.com/dexterai-lab/quantumdl',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/dexterai-lab/quantumdl/archive/v0.1-alpha.tar.gz',    # I explain this later on
-  keywords = ['QUANTUM COMPUTING', 'DEEP LEARNING', 'PYTHON'],   # Keywords that define your package best
+  keywords = ['QUANTUM COMPUTING', 'DEEP LEARNING', 'ARTIFICIAL INTELLIGENCE', 'PYTHON' ],   # Keywords that define your package best
   python_requires=('>=3.6.0'),
   install_requires=[            # I get to this in a second
           'cirq'
@@ -17,7 +25,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3.0'   # Again, pick a license
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'   # Again, pick a license
   ],
 )
