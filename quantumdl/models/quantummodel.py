272b25af14b67b0ada33629a320d812df831759b
dexterai-lab <debabrata.rc91@gmaill.com>
2020-08-30 18:03:52 +0200

    Initial commit for models.

---
+++ quantumdl/models/quantummodel.py
@@ -0,0 +1,8 @@
+import cirq as cq
+import quantumdl.core.engine as engine
+
+class Model():
+    """This is a class to Handle Quantum model creation"""
+    def __init__(self, name):
+        self.param = self
+        self.state = None
